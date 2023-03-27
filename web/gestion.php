e0dbbb9ca33f70e4c8ad283a0df445704b1c0910
diveaire <59625132+diveaire@users.noreply.github.com>
2023-03-27 17:52:35 +0200

    Update gestion.php

--- web/gestion.php
+++ web/gestion.php
@@ -20,10 +20,68 @@ $metier=$_SESSION['metier'];
 <ul id="menu">
     <li class="menu_elm"><a class="menuLink" href="accueil.php">Accueil</a></li>
     <li class="menu_elm"><a class="menuLink" href="profil.php">Profil</a></li>
-    <li class="menu_elm"><a class="menuLink" href="recherche.old.php">Recherche</a></li>
+    <li class="menu_elm"><a class="menuLink" href="recherche.php">Recherche</a></li>
     <li class="menu_elm"><a class="menuLink" href="gestion.php">Gestion Administrative</a></li>
     <li id="logout" ><a class="menuLink" href="logout.php">Log out</a></li>
 </ul>
+
+<div class='bloc'>
+	<div class='group'>Recherche</div>
+</div>
+<div class='bloc'>
+	<form action='gestion.php' method='POST'>
+	<div class='bloc'>
+		<div class='group'>Manège
+			<input class='modif' type='submit' name='manege' value='Modifier'>
+		</div>
+		<?PHP
+			$req="SELECT nomM FROM Manege";
+			$res=mysqli_query($idcom,$req);
+			if($res){
+				echo "<table border=2px>";
+				while($row=mysqli_fetch_array($res)){
+					echo "<tr><td><a href='manegeadm.php?$row[0]'>$row[0]</a></td></tr>";
+				}
+				echo "</table>";
+			}
+		?>
+	</div>
+	<div class='bloc'>
+		<div class='group'>Boutique
+			<input class='modif' type='submit' name='manege' value='Modifier'>
+		</div>
+		<?PHP
+			$req="SELECT IdB,nomB FROM Boutique";
+			$res=mysqli_query($idcom,$req);
+			if($res){
+				echo "<table border=2px>";
+				while($row=mysqli_fetch_array($res)){
+					echo "<tr><td>$row[0]</td><td><a href='boutiqueadm.php?$row[0]'>$row[1]</a></td></tr>";
+				}
+				echo "</table>";
+			}
+		?>
+	</div>
+	<div class='bloc'>
+		<div class='group'>Atelier
+			<input class='modif' type='submit' name='manege' value='Modifier'>
+		</div>
+		<?PHP
+			$req="SELECT IdA,nomA FROM Atelier";
+			$res=mysqli_query($idcom,$req);
+			if($res){
+				echo "<table border=2px>";
+				while($row=mysqli_fetch_array($res)){
+					echo "<tr><td>$row[0]</td><td><a href='atelieradm.php?$row[0]'>$row[1]</a></td></tr>";
+				}
+				echo "</table>";
+			}
+		?>
+	</div>
+	</form>
+</div>
+
+
 
 </body>
 </html>
